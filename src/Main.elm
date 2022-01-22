2ffcdc08653664aa07898dba7bac07c1bab7dee4
Yuya Kono <takohati0821@gmail.com>
2022-01-22 18:53:08 +0900

    Use flexsearch

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,4 @@
-module Main exposing (..)
+port module Main exposing (..)
 
 import Browser
 import Element exposing (..)
@@ -7,10 +7,7 @@ import Element.Input as Input
 import ElmUiRenderer exposing (elmUiRenderer)
 import Hovercraft exposing (Hovercraft, hovercraftDecoder)
 import Html exposing (Html)
-import Http
-import Json.Decode as Json exposing (Decoder, list, succeed)
-import Json.Decode.Pipeline exposing (required)
-import Json.Encode as E
+import Json.Decode as Json 
 import Markdown.Parser as Markdown
 import Markdown.Renderer as Markdown
 
@@ -19,14 +16,16 @@ main : Program () Model Msg
 main =
     Browser.element { init = init, update = update, view = view, subscriptions = subscriptions }
 
+port requestSearch : String -> Cmd msg
+port searchReceiver : (List Json.Value -> msg) -> Sub msg
 
 type alias Model =
-    { query : String, hits : List Hit }
+    { query : String, hits : List (Result Json.Error Hovercraft) }
 
 
 type Msg
     = Change String
-    | Hits (Result Http.Error (List Hit))
+    | RecvSearch (List (Result Json.Error Hovercraft))
 
 
 init : () -> ( Model, Cmd Msg )
@@ -41,80 +40,20 @@ update msg model =
             ( { model
                 | query = newQuery
               }
-            , fetchSearchResults newQuery
+            , requestSearch newQuery
             )
 
-        Hits (Ok hits) ->
+        RecvSearch hits ->
             ( { model
                 | hits = hits
               }
             , Cmd.none
             )
 
-        Hits (Err err) ->
-            let
-                _ =
-                    Debug.log "Err" (Debug.toString err)
-            in
-            ( model, Cmd.none )
-
-
-fetchSearchResults : String -> Cmd Msg
-fetchSearchResults query =
-    Http.request
-        { method = "POST"
-        , headers = []
-        , url = "/api/hovercraft/_search"
-        , body =
-            Http.jsonBody
-                (E.object
-                    [ ( "size", E.float 10000 )
-                    , ( "query"
-                      , E.object
-                            [ ( "match"
-                              , E.object
-                                    [ ( "hover.contents.value"
-                                      , E.object [ ( "query", E.string (Debug.log "query" query) ), ( "fuzziness", E.string "AUTO" ) ]
-                                      )
-                                    ]
-                              )
-                            ]
-                      )
-                    ]
-                )
-        , expect = Http.expectJson Hits hitsDecoder
-        , timeout = Nothing
-        , tracker = Nothing
-        }
-
-
-
--- | Hit data from ElasticSearch.
-
-
-type alias Hit =
-    { source : Hovercraft }
-
-
-
--- | The decoder for the JSON response from ElasticSearch.
-
-
-hitsDecoder : Decoder (List Hit)
-hitsDecoder =
-    succeed identity
-        |> required "hits" (succeed identity |> required "hits" (list hitDecoder))
-
-
-hitDecoder : Decoder Hit
-hitDecoder =
-    succeed Hit
-        |> required "_source" hovercraftDecoder
-
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-    Sub.none
+    searchReceiver (\hits -> RecvSearch (List.map (Json.decodeValue hovercraftDecoder) hits))
 
 
 view : Model -> Html Msg
@@ -131,21 +70,24 @@ view model =
             )
 
 
-viewHit : Hit -> Element Msg
+viewHit : Result Json.Error Hovercraft -> Element Msg
 viewHit hit =
-    case Markdown.parse hit.source.hover.contents.value of
-        Ok contents ->
-            case Markdown.render elmUiRenderer contents of
-                Ok rendered ->
-                    column
-                        [ Border.width 2
-                        , Border.rounded 6
-                        , Border.color <| rgb255 0xC0 0xC0 0xC0
-                        ]
-                        (List.map (el [ paddingXY 10 0 ]) rendered)
+    case hit of
+        Ok hovercraft ->
+            case Markdown.parse hovercraft.hover.contents.value of
+                Ok contents ->
+                    case Markdown.render elmUiRenderer contents of
+                        Ok rendered ->
+                            column
+                                [ Border.width 2
+                                , Border.rounded 6
+                                , Border.color <| rgb255 0xC0 0xC0 0xC0
+                                ]
+                                (List.map (el [ paddingXY 10 0 ]) rendered)
+
+                        Err err ->
+                            text err
 
                 Err err ->
-                    text err
-
-        Err err ->
-            column [] (List.map (\e -> text (Markdown.deadEndToString e)) err)
+                    column [] (List.map (\e -> text (Markdown.deadEndToString e)) err)
+        Err err -> text (Json.errorToString err)
--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+'use strict';
 import { Elm } from "./Main.elm";
+import { Document } from "flexsearch";
+import hovercrafts from "../data/hovercraft.json";
 
-Elm.Main.init({ node: document.getElementById("root") });
+var app = Elm.Main.init({ node: document.getElementById("root") });
+
+const index = new Document({
+	encode: function (str) {
+		return str.toLowerCase().split(/\s+/g);
+	},
+	tokenize: "forward",
+	document: {
+		id: "id",
+		index: ["contents:hover:contents:value"],
+	}
+});
+
+for (let i = 0; i < hovercrafts.length; i++) {
+	index.add({ id: i, contents: hovercrafts[i] });
+}
+
+app.ports.requestSearch.subscribe(function (query) {
+	console.log("requestSearch", query);
+
+	const results = index.search(query, { pluck: "contents:hover:contents:value" })
+		.map((id) => hovercrafts[id]);
+	console.log("results", results);
+	app.ports.searchReceiver.send(results);
+});
