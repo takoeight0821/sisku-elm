94fa6bac64d2448ff31bdba866142ff7386a7edc
Yuya Kono <takohati0821@gmail.com>
2022-03-28 16:54:08 +0900

    Use elm-explorations/markdown

--- src/Main.elm
+++ src/Main.elm
@@ -6,13 +6,11 @@ import Element exposing (..)
 import Element.Border as Border
 import Element.Input as Input
 import Element.Lazy as Lazy
-import ElmUiRenderer exposing (elmUiRenderer)
 import Hovercraft exposing (Entry, entryDecoder)
 import Html exposing (Html)
 import Html.Events
 import Json.Decode as Json
-import Markdown.Parser as Markdown
-import Markdown.Renderer as Markdown
+import Markdown
 import RemoteData exposing (RemoteData(..))
 
 
@@ -180,51 +178,53 @@ onEnter msg =
 view : Model -> Html Msg
 view model =
     layout [ padding 30 ] <|
-        column [ alignTop, spacing 7 ] <|
-            List.map
-                (\projectId ->
-                    Input.checkbox []
-                        { onChange = Contains projectId
-                        , icon = Input.defaultCheckbox
-                        , checked = Maybe.withDefault False (Dict.get projectId model.projectIds)
-                        , label = Input.labelRight [] <| text projectId
-                        }
-                )
-                (Dict.keys model.projectIds)
-                ++ [ Input.text
-                        [ Input.focusedOnLoad, onEnter EnterWasPressed ]
-                        { onChange = ChangeQuery
-                        , text = model.query
-                        , placeholder = Just <| Input.placeholder [] <| text "Type here and press enter"
-                        , label = Input.labelLeft [] <| text "Text to search"
-                        }
-                   , Input.text
-                        []
-                        { onChange = ChangePlaceholder
-                        , text = model.placeholder
-                        , placeholder = Just <| Input.placeholder [] <| text "_"
-                        , label = Input.labelLeft [] <| text "Placeholder"
-                        }
-                   , Input.checkbox []
-                        { onChange = FuzzMode
-                        , icon = Input.defaultCheckbox
-                        , checked = model.isFuzzMode
-                        , label = Input.labelRight [] <| text "Fuzzy search"
-                        }
-                   ]
-                ++ (case model.hits of
-                        NotAsked ->
-                            []
+        column [ spacing 7, width fill ]
+            [ column [] <|
+                List.map
+                    (\projectId ->
+                        Input.checkbox []
+                            { onChange = Contains projectId
+                            , icon = Input.defaultCheckbox
+                            , checked = Maybe.withDefault False (Dict.get projectId model.projectIds)
+                            , label = Input.labelRight [] <| text projectId
+                            }
+                    )
+                    (Dict.keys model.projectIds)
+            , column [ width fill ]
+                [ Input.text
+                    [ Input.focusedOnLoad, onEnter EnterWasPressed ]
+                    { onChange = ChangeQuery
+                    , text = model.query
+                    , placeholder = Just <| Input.placeholder [] <| text "Type here and press enter"
+                    , label = Input.labelLeft [] <| text "Text to search"
+                    }
+                , Input.text
+                    []
+                    { onChange = ChangePlaceholder
+                    , text = model.placeholder
+                    , placeholder = Just <| Input.placeholder [] <| text "_"
+                    , label = Input.labelLeft [] <| text "Placeholder"
+                    }
+                , Input.checkbox []
+                    { onChange = FuzzMode
+                    , icon = Input.defaultCheckbox
+                    , checked = model.isFuzzMode
+                    , label = Input.labelRight [] <| text "Fuzzy search"
+                    }
+                ]
+            , case model.hits of
+                NotAsked ->
+                    text ""
 
-                        Loading ->
-                            [ text "Loading..." ]
+                Loading ->
+                    text "Loading..."
 
-                        Failure _ ->
-                            [ text "ERROR" ]
+                Failure _ ->
+                    text "ERROR"
 
-                        Success hits ->
-                            List.map (Lazy.lazy viewHit) hits
-                   )
+                Success hits ->
+                    column [ width fill ] <| List.map (Lazy.lazy viewHit) hits
+            ]
 
 
 viewHit : Result Json.Error Entry -> Element Msg
@@ -233,21 +233,17 @@ viewHit hit =
         |> Result.mapError (Json.errorToString >> text)
         |> Result.andThen
             (\entry ->
-                Markdown.parse entry.hover.contents.value
-                    |> Result.mapError (List.map (Markdown.deadEndToString >> text) >> column [])
-                    |> Result.map (\markdown -> ( entry.projectId, markdown ))
+                Ok ( entry.projectId, entry.hover.contents.value )
             )
         |> Result.andThen
             (\( projectId, markdown ) ->
-                Markdown.render elmUiRenderer markdown
-                    |> Result.mapError text
-                    |> Result.map (List.append [ text projectId ])
+                Ok <| column [] [ html (Markdown.toHtml [] markdown), text projectId ]
             )
         |> (\r ->
                 case r of
-                    Ok results ->
-                        column [ Border.width 2, Border.rounded 6, Border.color <| rgb255 0xC0 0xC0 0xC0 ] <|
-                            List.map (el [ paddingXY 10 0 ]) results
+                    Ok result ->
+                        column [ Border.width 2, Border.rounded 6, Border.color <| rgb255 0xC0 0xC0 0xC0 ]
+                            [ el [ paddingXY 10 0 ] result ]
 
                     Err elem ->
                         elem
